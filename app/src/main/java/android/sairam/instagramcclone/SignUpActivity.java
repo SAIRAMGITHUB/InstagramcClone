89ae60f1f68f283b1a8dba2b9900da01ecb8a16a
Sairam <venkatasairam@outlook.com>
2018-12-21 11:45:21 +0800

    get all kickboxers from Parse KickBoxer class

--- app/src/main/java/android/sairam/instagramcclone/SignUpActivity.java
+++ app/src/main/java/android/sairam/instagramcclone/SignUpActivity.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
+
+import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -52,6 +56,39 @@ public class SignUpActivity extends AppCompatActivity {
             FancyToast.makeText(SignUpActivity.this,e.getMessage(), FancyToast.LENGTH_SHORT,FancyToast.ERROR,true).show();
 
         }
+
+    }
+
+
+    public  void  getDataTextViewTapped(View view) {
+
+//        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("KickBoxer");
+//        parseQuery.getInBackground("vZH9T0BlA9", new GetCallback<ParseObject>() {
+//            @Override
+//            public void done(ParseObject object, ParseException e) {
+//                if (object != null && e == null) {
+//                    FancyToast.makeText(SignUpActivity.this, object.get("name") + "", FancyToast.LENGTH_SHORT,FancyToast.SUCCESS,true).show();
+//                }
+//                else {
+//                    FancyToast.makeText(SignUpActivity.this,e.getMessage(), FancyToast.LENGTH_SHORT,FancyToast.ERROR,true).show();
+//                }
+//            }
+//        });
+
+        ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("KickBoxer");
+        parseQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+
+                String names = "";
+                for (ParseObject object:objects) {
+                    names = names + object.get("name") + " ";
+                }
+                FancyToast.makeText(SignUpActivity.this, names, FancyToast.LENGTH_SHORT,FancyToast.SUCCESS,true).show();
+
+            }
+        });
+
 
     }
 }
